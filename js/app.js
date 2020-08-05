a61b6fc45b489d05effcc905156c4985e4650dfc
Mary Pieroszkiewicz <mary.pieroszkiewicz@gmail.com>
2020-08-05 09:51:01 +0200

    add files

---
+++ js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  /* ---------------------------------------------------- */
+  /* ----- TOGGLE HEADER MENU ----- */
+  /* ---------------------------------------------------- */
+
+  const btn = document.querySelector('#btnToggle');
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.body.classList.toggle('menu--show');
+    this.classList.toggle('btn-toggle--toggled');
+  });
+
+
+  /* ---------------------------------------------------- */
+  /* ----- HIGHLIGHT CURRENT PAGE ACTIVE MENU ITEM ----- */
+  /* ---------------------------------------------------- */
+
+  const items = document.querySelectorAll('.menu__link');
+
+  function makeActive() {
+
+    items.forEach(elem => elem.classList.remove('active'));
+    this.classList.add('active');
+
+  }
+
+  items.forEach(elem => {
+    elem.addEventListener('click', makeActive);
+  });
+
+})
