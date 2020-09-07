82a4ab7430e9aaa148af646135d076c996305ff0
Mary Pieroszkiewicz <mary.pieroszkiewicz@gmail.com>
2020-09-07 10:46:24 +0200

    add stretch rubber animation ScrollTrigger GSAP

---
+++ js/animations.js
@@ -0,0 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// You can use a ScrollTrigger in a tween or timeline
+gsap.to(".rubber", {
+  scaleX: 1.2,
+  scrollTrigger: {
+    trigger: ".rubber",
+    start: "top center",
+//    end: "+=500",
+    pin: false,
+//    scrub: true,
+    markers: true
+  }
+});
