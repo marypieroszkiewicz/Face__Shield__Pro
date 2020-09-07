fc3aad57a3a0368c2b29c613dae6958d0cbe4e99
Mary Pieroszkiewicz <mary.pieroszkiewicz@gmail.com>
2020-09-07 09:51:24 +0200

    add changes

---
+++ js/cookie.js
@@ -0,0 +1,13 @@
+const cookieContainer = document.querySelector(".cookie");
+const cookieButton = document.querySelector(".cookie__ctn-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
